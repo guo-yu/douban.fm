cab8450660001ba2b5a41b27e94d13959df148df
Zhuang Ya <zhuangya@gmail.com>
2014-01-15 01:56:21 +0800

    add prompt for better email and password input.
    如果是使用 process.argv 来获取用户名和密码的话，可能又有好多人要泄
    漏豆瓣密码了 url: http://seclists.org/fulldisclosure/2013/Jun/15

--- libs/cli.js
+++ libs/cli.js
@@ -1,5 +1,6 @@
 var consoler = require('consoler'),
     optimist = require('optimist'),
+    prompt = require('prompt'),
     argv = optimist.argv,
     Fm = require('./fm');
 
@@ -7,18 +8,41 @@ exports = module.exports = function() {
     var home = argv.h ? argv.h : null;
     var argument = argv._,
         fm = new Fm(home);
+
+    var promptSchema = {
+      properties: {
+        email: {
+          //via: https://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+          pattern: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          message: '电子邮箱格式有错误',
+          required: true
+        },
+        password: {
+          hidden: true,
+          required: true
+        }
+      }
+    };
+
     var ready = function() {
         consoler.loading('正在加载...');
     };
-    if (!argv.m) return fm.init(ready);
-    if (argument.length != 1) return consoler.error('请输入正确的豆瓣电台账户密码，以空格分割');
-    fm.auth({
-        email: argv.m,
-        password: argument[0]
-    }, function(err, configs) {
-        if (err) return consoler.error(err);
-        var user = configs.account;
-        consoler.success('欢迎你，' + user.user_name + '。您的豆瓣账户已经成功修改为：' + user.email);
-        fm.init(ready);
-    });
+    if (!argv.m) {
+      return fm.init(ready);
+    } else {
+
+      prompt.start();
+
+      prompt.get(promptSchema, function (err, result) {
+        fm.auth({
+          email: result.email,
+          password: result.password
+        }, function (err, configs) {
+          if (err) return consoler.error(err);
+          var user = configs.account;
+          consoler.success('欢迎你，' + user.user_name + '。您的豆瓣账户已经成功修改为：' + user.email);
+          fm.init(ready);
+        });
+      });
+    }
 };
