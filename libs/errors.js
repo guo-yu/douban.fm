3f2c446800864d9a2638ea0667329c3a69c576b1
Guo Yu <i@guoyu.me>
2014-01-17 13:38:47 +0800

    0.1.1: 发布 0.1.1
    * 一并修复了获取列表时切换频道出现重复播放的情况
    * 添加停止播放提示

--- libs/errors.js
+++ libs/errors.js
@@ -2,5 +2,5 @@ module.exports = {
     "invalidate_email": "抱歉，您的豆瓣帐号似乎出错了",
     "wrong_email": "抱歉，您的豆瓣帐号似乎出错了",
     "wrong_password": "抱歉，您的豆瓣密码似乎出错了",
-    "account_missing": "请先设置豆瓣账户再收听私人兆赫哦~ $ douban.fm -m [account] [password]"
+    "account_missing": "请先设置豆瓣账户再收听私人/红心兆赫: $ douban.fm config"
 };
