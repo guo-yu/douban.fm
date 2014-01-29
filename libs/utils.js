7f7ed9e366c06d0429d28a304bf9fa51073b7868
guo-yu <o.u.turing@gmail.com>
2014-01-30 00:30:34 +0800

    0.1.2: 初步支持本地电台

--- libs/utils.js
+++ libs/utils.js
@@ -1,4 +1,5 @@
-var exeq = require('exeq'),
+var fs = require('fs'),
+    exeq = require('exeq'),
     open = process.platform === 'win32' ? 'start' : 'open';
 
 // 获取用户的家地址
@@ -9,11 +10,29 @@ exports.home = function() {
 // 跳转到相应页面，使用 open 或者 start
 exports.go = function(link) {
     if (!link) return false;
-    return exeq([ open + ' ' + link ]).run();
+    return exeq([open + ' ' + link]).run();
 }
 
 // 解析歌曲专辑页面可能出现的小站链接
 exports.album = function(link) {
     if (!link) return false;
     return link.indexOf('http') === -1 ? 'http://music.douban.com' + link : link;
+}
+
+// 读写json
+exports.json = function(file, callback, contents) {
+    if (!contents) {
+        return fs.readFile(file, function(err, data) {
+            if (err) return callback(err, null);
+            try {
+                callback(err, JSON.parse(data));
+            } catch (err) {
+                callback(err);
+            }
+        });
+    } else {
+        return fs.writeFile(file, JSON.stringify(contents), function(err) {
+            callback(err, contents);
+        });
+    }
 }
