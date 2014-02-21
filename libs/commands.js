53154c7872f4c2a33489fd8f4b18f9f6db2ff2c3
Guo Yu <i@guoyu.me>
2014-02-21 23:33:42 +0800

    0.1.2: 支持自定义下载目录，更换配置文件地址

---
+++ libs/commands.js
@@ -0,0 +1,80 @@
+var consoler = require('consoler'),
+    prompt = require('prompt'),
+    Fm = require('./fm');
+
+var promptSchema = {
+    properties: {
+        email: {
+            description: 'Douban Email',
+            type: 'string',
+            pattern: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: '电子邮箱格式有错误',
+            required: true
+        },
+        password: {
+            description: 'Douban Password',
+            hidden: true,
+            required: true
+        }
+    }
+};
+
+exports.ready = function() {
+    return consoler.loading('正在加载...');
+};
+
+exports.config = function(fm) {
+    prompt.start();
+    prompt.get(promptSchema, function(err, result) {
+        if (err) return consoler.error(err);
+        fm.auth({
+            email: result.email,
+            password: result.password
+        }, function(err, configs) {
+            if (err) return consoler.error(err);
+            var user = configs.account;
+            consoler.success('欢迎你，' + user.user_name + '。您的豆瓣账户已经成功修改为：' + user.email);
+            fm.init(exports.ready);
+        });
+    });
+};
+
+exports.home = function(fm, argv) {
+    fm.configs(function(err, profile) {
+        if (err) return consoler.error(err);
+        var selected = argv[3];
+        profile.home = selected || process.cwd();
+        fm.configs(profile, function(err, pro) {
+            if (err) return consoler.error(err);
+            consoler.success('下载目录已成功修改为' + pro.home);
+            var f = new Fm;
+            return f.init(exports.ready);
+        });
+    });
+};
+
+exports.help = function() {
+    console.log('');
+    consoler.align(4);
+    consoler.info('豆瓣电台命令行版帮助文档');
+    return console.log([
+        "",
+        "更新豆瓣电台命令行版：",
+        "$ [sudo] npm install douban.fm -g",
+        "",
+        "配置豆瓣账户密码：",
+        "$ douban.fm config",
+        "",
+        "菜单快捷键：",
+        "[return]      ->     播放另一个频道，或者重新播放当前频道 (PLAY)",
+        "[backspace]   ->     停止播放当前歌曲或频道 (DELETE)",
+        "[n]           ->     本频道列表的下一首歌曲 (NEXT)",
+        "[l]           ->     添加到红心列表或者删除红心 (LOVE)",
+        "[s]           ->     分享当前歌曲到新浪微博 (SHARE)",
+        "[r]           ->     开启或关闭歌词，默认关闭歌词显示 (LRC)",
+        "[g]           ->     跳转到当前播放歌曲的专辑页面 (GOTO)",
+        "[r]           ->     切换歌词显示 (LRC)",
+        "[q]           ->     退出豆瓣电台 (QUIT)",
+        ""
+    ].join('\n'));
+};
