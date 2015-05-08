c9e3b95c89597aeb4579cab7426db9834e2e5f81
guo-yu <o.u.turing@gmail.com>
2015-05-08 20:34:53 +0800

    Rewrite lib utils with ES6

--- libs/utils.js
+++ libs/utils.js
@@ -1,74 +1,46 @@
-var fs = require('fsplus');
+import fs from 'fsplus'
 
-exports.sid = sid;
-exports.album = album;
-exports.readJSON = readJSON;
-exports.isFunction = isFunction;
-exports.noSuchFile = noSuchFile;
-
-/**
- * [Escape a douban site uri from normal uri.]
- * @param  {[type]} link [description]
- * @return {[type]}      [description]
- */
-function album(link) {
+// Escape a douban site uri from normal URI
+export function album(link) {
   if (!link) 
-    return false;
+    return
 
-  var str = link.indexOf('http') === -1 ? 
+  return link.indexOf('http') === -1 ? 
     'http://music.douban.com' + link : 
-    link;
-
-  return str;
+    link
 }
 
-/**
- * [Split Sid from a song's title]
- * @param  {[type]} filename [description]
- * @return {[type]}          [description]
- */
-function sid(filename) {
+// Split Sid from a song's title
+export function sid(filename) {
   if (!filename) 
-    return false;
+    return
 
   var idstr = filename.substr(
     filename.indexOf('p') + 1, 
     filename.lastIndexOf('.') - 1
-  );
+  )
 
   if (idstr.indexOf('_') === -1) 
-    return idstr;
+    return idstr
 
-  return idstr.substr(0, idstr.lastIndexOf('_'));
+  return idstr.substr(0, idstr.lastIndexOf('_'))
 }
 
-/**
- * [Read JSON file, if Error return blank object]
- * @param  {[type]} file [description]
- * @return {[type]}      [description]
- */
-function readJSON(file) {
+// Read JSON file, if Error return blank object
+export function readJSON(file) {
   try {
-    return fs.readJSON(file);
+    return fs.readJSON(file)
   } catch (err) {
-    return {};
+    return {}
   }
 }
 
-/**
- * [Check if a object is Function Type]
- * @param  {[type]}  func [description]
- * @return {Boolean}      [description]
- */
-function isFunction(func) {
-  return func && typeof(func) === 'function';
+// Check if a object is Function Type
+export function isFunction(func) {
+  return func && typeof(func) === 'function'
 }
 
-/**
- * [Check if a Error is a NoSuchFile Error]
- * @param  {[type]} msg [description]
- * @return {[type]}     [description]
- */
-function noSuchFile(msg) {
-  return msg && msg.indexOf('no such file or directory') > -1;
+// Check if a Error is a NoSuchFile Error
+export function noSuchFile(msg) {
+  return msg && msg.indexOf('no such file or directory') > -1
 }
